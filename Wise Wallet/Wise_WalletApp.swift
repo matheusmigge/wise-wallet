ad3e53a31322fe4d76a4611e70020df5e6c4ffec
Matheus Migge <matheusmigge@gmail.com>
2024-01-10 17:04:15 -0300

    Initial Commit

---
+++ Wise Wallet/Wise_WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wise_WalletApp.swift
+//  Wise Wallet
+//
+//  Created by Matheus Migge on 10/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Wise_WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
